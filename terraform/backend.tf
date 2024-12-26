9b777533ed0a671a09f87fce8e86cc805a230505
Devesh08yadav <ydevesh08@gmail.com>
2024-12-26 18:40:47 +0530

    deploy changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state"
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-northeast-1"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "AWS region to deploy resources in"
-  default     = "us-east-1"
+  default     = "ap-northeast-1"
 }
 
 variable "bucket_name" {
