c6cabe5f830d65a75bc7ee778a91c7b6aa7e250c
Devesh08yadav <ydevesh08@gmail.com>
2024-12-26 08:48:56 +0530

    added workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state"
     key            = "state/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "ap-northeast-1"
   }
 }
